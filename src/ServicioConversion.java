62cd82dd6a73538aa233f664ed1179f8da851102
luisssuki <67665604+luisssuki@users.noreply.github.com>
2024-07-16 16:20:44 -0600

    Update ServicioConversion.java

--- src/ServicioConversion.java
+++ src/ServicioConversion.java
@@ -7,32 +7,29 @@ import java.net.http.HttpResponse;
 
 public class ServicioConversion {
 
-    public TasaCambio obtenerTasa(String divisaBase, String divisaObjetivo) {
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/40803f00e2546baaa64e3151/pair/" + divisaBase
-                + "/" + divisaObjetivo);
+  public TasaCambio obtenerTasa(String divisaBase, String divisaObjetivo) {
+    URI direccion = URI.create("https://v6.exchangerate-api.com/v6/3bd94cd92e293227a4fae0a0/latest/USD" + divisaBase +
+      "/" + divisaObjetivo);
 
-        HttpClient cliente = HttpClient.newHttpClient();
-        HttpRequest solicitud = HttpRequest.newBuilder()
-                .uri(direccion)
-                .build();
+    HttpClient cliente = HttpClient.newHttpClient();
+    HttpRequest solicitud = HttpRequest.newBuilder()
+      .uri(direccion)
+      .build();
 
-        try {
-            HttpResponse<String> respuesta = cliente.send(solicitud, HttpResponse.BodyHandlers.ofString());
+    try {
+      HttpResponse < String > respuesta = cliente.send(solicitud, HttpResponse.BodyHandlers.ofString());
 
-            // Verificar si la solicitud fue exitosa
-            if (respuesta.statusCode() == 200) {
-                return new Gson().fromJson(respuesta.body(), TasaCambio.class);
-            } else {
-                System.out.println("La solicitud HTTP no fue exitosa: " + respuesta.statusCode());
-                return null;
-            }
-        } catch (IOException | InterruptedException e) {
-            // Manejar la excepción de manera adecuada
-            e.printStackTrace();
-            return null;
-        } finally {
-            // Cerrar el cliente HttpClient para liberar los recursos
-            cliente.close();
-        }
+      if (respuesta.statusCode() == 200) {
+        return new Gson().fromJson(respuesta.body(), TasaCambio.class);
+      } else {
+        System.out.println("La solicitud HTTP no fue exitosa: " + respuesta.statusCode());
+        return null;
+      }
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+      return null;
+    } finally {
+      cliente.close();
     }
+  }
 }
